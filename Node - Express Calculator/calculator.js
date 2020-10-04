a118ec109bfe26375d3d567a7ec87e722fc4c1d4
ryantoddgarza <ryantoddgarza@gmail.com>
2020-10-04 16:42:07 -0700

    completing bmi calculator

--- Node - Express Calculator/calculator.js
+++ Node - Express Calculator/calculator.js
@@ -18,6 +18,33 @@ app.post('/', (req, res) => {
   res.send(`The result of the calculation is ${result}`);
 });
 
+app.get('/bmi', (req, res) => {
+  res.sendFile(`${__dirname}/bmi.html`);
+});
+
+app.post('/bmi', (req, res) => {
+  const weight = Number(req.body.weight);
+  const height = Number(req.body.height);
+
+  const bmiCalculator = (w, h) => {
+    let bmi = Math.round(w / Math.pow(h, 2));
+
+    if (bmi < 18.5) {
+      return `Your BMI is ${bmi}, so you are underweight.`;
+    }
+    if (bmi >= 18.5 && bmi <= 24.9) {
+      return `Your BMI is ${bmi}, so you have a normal weight.`;
+    }
+    if (bmi > 24.9) {
+      return `Your BMI is ${bmi}, so you are overweight.`;
+    }
+  };
+
+  const result = bmiCalculator(weight, height);
+
+  res.send(result);
+});
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
